74b246ff41072c15908ab48ab26ad3d8a6af0e01
rad <iamsumankht@gmail.com>
2023-03-25 20:25:27 +0545

    Proto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./utils/Layout";
 import Home from "./pages/Home";
